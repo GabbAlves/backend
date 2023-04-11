2e157b83fd8361b97859d1ae4f05d64dbc539028
GabbAlves <alucar725@gmail.com>
2023-04-10 22:13:02 -0300

    comecei a fazer para buscar imagem

--- src/main/java/com/GOATstore/backend/repository/ProdutoRepository.java
+++ src/main/java/com/GOATstore/backend/repository/ProdutoRepository.java
@@ -13,4 +13,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
       @Query(" SELECT * FROM produto p INNER JOIN marca m ON p.id_marca = m.id INNER JOIN categoria c ON p.id_categoria = c.id WHERE m.nome = ?1 OR c.nome = ?2 p.nome = ?3")
       List<Produto> buscarPorReferencia(String marca, String categoria, String nome);
     
+
+      @Query(" SELECT * FROM produto p INNER JOIN marca m ON p.id_marca = m.id INNER JOIN produtoImagens c ON p.id_categoria = c.id INNER JOIN categoria c ON p.id_categoria WHERE m.nome = ?1 OR c.nome = ?2 p.nome = ?3")
+      List<Produto> buscarProdutoFoto(String marca, String categoria, String nome);
 }
