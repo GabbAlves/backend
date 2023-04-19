d64012e5165810e1c94b50025b01946f5512c984
GabbAlves <alucar725@gmail.com>
2023-04-18 22:12:48 -0300

    filtro de produtos

--- src/main/java/com/GOATstore/backend/controller/ProdutoController.java
+++ src/main/java/com/GOATstore/backend/controller/ProdutoController.java
@@ -1,5 +1,6 @@
 package com.GOATstore.backend.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,39 +22,56 @@ import com.GOATstore.backend.service.ProdutoService;
 @RestController
 @RequestMapping("/api/produto")
 public class ProdutoController {
-    
+
     @Autowired
     private ProdutoService produtoService;
     @Autowired
     private ProdutoRepository produtoRepository;
 
     @GetMapping("/")
-    public List<Produto> buscarTodos(){
-     return produtoService.buscarTodos();
+    public List<Produto> buscarTodos() {
+        return produtoService.buscarTodos();
     }
 
     @GetMapping("/Lupa")
-        public List<Produto> buscarProduto(@RequestParam String marca, @RequestParam String categoria, @RequestParam String nome){
-         return   produtoRepository.buscarPorReferencia(marca, categoria, nome);
+    public List<Produto> buscarProduto(@RequestParam String marca, @RequestParam String categoria,
+            @RequestParam String nome) {
+        return produtoRepository.buscarPorTodos(marca, categoria, nome);
+
+    }
+
+    @GetMapping("/Filtro")
+    public List<Produto> buscarPorFiltro(@RequestParam(required = false) String marca,
+            @RequestParam(required = false) String categoria) {
+        if (marca != "" && categoria != "") {
+            return produtoRepository.buscarPorMarcaECategoria(marca, categoria);
+        } else if (marca != "" || categoria != "") {
+            return produtoRepository.buscarPorMarcaOuCategoria(marca, categoria);
+        } else {
+            return new ArrayList<Produto>();
         }
 
+    }
+
+    @GetMapping("/{id}")
+    public List<Produto> buscarPorId(@PathVariable("id") Long id) {
+        return produtoRepository.buscarPorId(id);
+    }
 
     @PostMapping("/")
-        public Produto inserir(@RequestBody Produto produto){
+    public Produto inserir(@RequestBody Produto produto) {
         return produtoService.inserir(produto);
     }
 
     @PutMapping("/")
-        public Produto alterar(@RequestBody Produto produto){
-            return produtoService.alterar(produto);
-        }
-    
+    public Produto alterar(@RequestBody Produto produto) {
+        return produtoService.alterar(produto);
+    }
 
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
-            produtoService.excluir(id);
-            return ResponseEntity.ok().build();
-        }
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
+        produtoService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
-
